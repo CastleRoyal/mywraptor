176b7d8480af18192d046cfaf81e56d1e25b69a9
admin <admin@DESKTOP-R6RSM54>
2023-06-15 13:45:23 +0900

    로그인 유효성체크 변경

--- prjt00/src/main/webapp/resources/js/checkMember.js
+++ prjt00/src/main/webapp/resources/js/checkMember.js
@@ -8,8 +8,7 @@ window.onload = function(){
 	let checkNn_btn = document.getElementById("checkNn_btn");
 	checkNn_btn.addEventListener("click", checkNickname);
 	
-	let checkNn_btn2 = document.getElementById("checkNn_btn2");
-	checkNn_btn2.addEventListener("click", checkNickname2)
+
 }	
 
 
@@ -35,16 +34,6 @@ function checkNickname(){
 	}
 }
 
-function checkNickname2(){
-	if(update_p.nickname.value.length ==0){
-		alert("닉네임을 입력해주세요.");
-		update_p.nickname.focus();
-		return false;
-	}else{
-		let url3 = "checkNickname2.jsp?nickname="+update_p.nickname.value;
-		window.open(url3, "_blank","toolbar=no,menubar=no,scrollbar=yes,resizable=no,width=500,height=200");
-	}
-}
 
 function checkInput(){
    
